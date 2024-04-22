8989005814d2c90f062e8d885c1eef57f5add2f3
Emil <emil.ahmadli@gmail.com>
2024-04-22 14:37:01 -0400

    updated readme to include training testing script, added trained models, updated model loading to be architecture agnostic.

--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
 import torch
 
+
 def get_last_inception_output_size(inception_out, inception_depth):
     return inception_out * 4 * (2 ** (inception_depth - 1))
+
 
 def save_model(model, path):
     torch.save(model, path)
 
+
 def load_model(path):
-    return torch.load(path)
+    return torch.load(path, map_location=torch.device('cpu'))
